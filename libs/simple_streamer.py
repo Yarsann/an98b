c9d22ab7b8f3281e40aa135991f93b5a2ace4cab
tomotomo <sugita@xshell.io>
2018-07-14 13:07:55 +0900

    save as avi file

--- libs/simple_streamer.py
+++ libs/simple_streamer.py
@@ -7,6 +7,10 @@ import cv2
 
 class SimpleStreamer(object):
     def __init__(self, flip = False):
+        # Define the codec and create VideoWriter object
+        fourcc = cv2.VideoWriter_fourcc(*'XVID')
+        self.out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
+        # Video Capture
         try:
             self.vc = cv2.VideoCapture(0)
         except:
@@ -15,11 +19,14 @@ class SimpleStreamer(object):
         time.sleep(2.0)
 
     def __del__(self):
+        self.out.release()
         self.vc.release()
 
     def get_output_image(self, frame):
         if self.flip:
             flipped_frame = cv2.flip(frame, 0)
+            # save as movie file
+            selft.out.write(flipped_frame)
             return cv2.imencode('.jpg', flipped_frame)
         return cv2.imencode('.jpg', frame)
 
