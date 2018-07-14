b84b16cb887d951bc8140608c69f2d1b93f04d14
tomotomo <sugita@xshell.io>
2018-07-14 12:32:31 +0900

    flip image

--- libs/simple_streamer.py
+++ libs/simple_streamer.py
@@ -8,22 +8,22 @@ import cv2
 class SimpleStreamer(object):
     def __init__(self, flip = False):
         try:
-            self.vs = cv2.VideoCapture(0)
+            self.vc = cv2.VideoCapture(0)
         except:
-            print(self.vs)
+            print(self.vc)
         self.flip = flip
         time.sleep(2.0)
 
     def __del__(self):
-        self.vs.release()
+        self.vc.release()
 
     def flip_if_needed(self, frame):
         if self.flip:
-            return np.flip(frame, 1)
+            return cv2.flip(frame, 0)
         return frame
 
     def get_frame(self):
-        ret, frame = self.vs.read()
+        ret, frame = self.vc.read()
         ret, jpeg = cv2.imencode('.jpg', frame)
         return jpeg.tobytes()
 
